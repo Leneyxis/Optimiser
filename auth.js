7fdaeb84493ab806c23cb6c4ae1376aeea81f8bd
Leneyxis <78430937+Leneyxis@users.noreply.github.com>
2024-10-15 21:27:02 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -22,7 +22,7 @@ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 // Function to send message to Chrome extension
 function sendMessageToExtension(userId) {
     chrome.runtime.sendMessage(
-        'fnjddgflihjpacfjdpehbnfmblhejpen',  // Replace with your extension's ID
+        'ipfmcnfggiceaflccbfcmagaclhldman',  // Replace with your extension's ID
         { action: 'storeUser', userId: userId },
         (response) => {
             console.log('Response from extension:', response);
